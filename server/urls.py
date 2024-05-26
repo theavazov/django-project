19b6289e524e9b95f5f3356e24990be92523758d
Doston Avazov <93529652+theavazov@users.noreply.github.com>
2024-05-26 18:21:07 +0500

    about page added to the client

--- server/urls.py
+++ server/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from client.views import index
+from client.views import index, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('about', about, name='about'),
 ]
